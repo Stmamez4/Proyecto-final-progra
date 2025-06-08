121949ce9a36ee384cb2979bf34c90847635a158
Misa G. <misael.estuardo.gonzalez@gmail.com>
2025-06-08 15:56:25 -0600

    estructura inicial con rutas actualizadas

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from modules.auth.routes import auth_blueprint
+from modules.books.routes import books_blueprint
+from modules.users.routes import users_blueprint
+from modules.loans.routes import loans_blueprint
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
+def create_app():
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    app.config.from_pyfile('config.py')
 
-class ExampleModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-
-@app.route('/')
-def home():
-    return {'message': '¡El servidor Flask está funcionando correctamente!'}
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(books_blueprint, url_prefix='/books')
+    app.register_blueprint(users_blueprint, url_prefix='/users')
+    app.register_blueprint(loans_blueprint, url_prefix='/loans')
+ 
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
