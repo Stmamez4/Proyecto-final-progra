a7d428f38faa02e3e687ddd829061d2602dd2009
Misa G. <misael.estuardo.gonzalez@gmail.com>
2025-06-12 17:42:12 -0600

    arreglo de bugs varios

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:pass@localhost:3307/bibliotecadb'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'mysql+pymysql://user:pass@127.0.0.1:3307/bibliotecadb'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
